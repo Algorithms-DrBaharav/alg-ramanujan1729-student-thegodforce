678049dc2483c0bb7be5061243db2393b8e10201
thegodforce <theultimatekamen@gmail.com>
2018-08-23 11:36:49 -0700

    Program works

--- src/Main1729.java
+++ src/Main1729.java
@@ -1,4 +1,4 @@
-
+import java.lang.Math;
 /**
  *
  * @author YourNameHere
@@ -11,3 +11,26 @@
 
 
 // Your work here. Just carete one class and one main function.
+public class Main1729{
+    public static void main(String[] args){
+        int goal=87539319;
+       int max_number=(int)Math.cbrt(goal);
+
+       
+       for(int x=0; x<=max_number; x++){
+           for(int y=0; y<=x; y++){
+               if (Math.pow(x, 3)+ Math.pow(y, 3) == goal){
+                   System.out.println("Potential numbers: " + x + ", " + y);
+               }
+               else{
+                   
+               }
+           }
+           
+       }
+       
+       
+    }
+    
+
+}
